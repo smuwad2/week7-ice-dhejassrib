9c7906304676f155cf787a8044b96b0956cfc69e
dhejassrib <dhejassri.b.2024@smu.edu.sg>
2025-10-22 02:34:32 +0000

    Ex2

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -1,18 +1,36 @@
 <script>
-    export default { 
-        // Add code in appropriate places
-        data() {
-            return {
-                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter']
-            }
-            
+export default { 
+    // Add code in appropriate places
+    data() {
+        return {
+            items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'],
+            newItem: ''
         }
+    },
+    methods: {
+        addItem() {
+            // push new item into items array
+            this.items.push(this.newItem.trim());
+            // this.newItem = ''; // clear input
+        }
+        // deleteItem(index) {
+        //     this.items.splice(index, 1);
+        // }
     }
+}
 </script>
 
 <template>
-
     <h2>Shopping Cart</h2>
-    <!-- TODO: Add Code Here -->
-   
+
+    <ul>
+        <li v-for="(item, idx) in items">
+            {{ item }}
+            <!-- <button @click=deleteItem(idx)>Delete!</button> -->
+            <button @click="items.splice(idx)">Delete!</button>
+        </li>
+    </ul>
+
+    <input type="text" v-model="newItem"></input>
+    <button @click="addItem">Add!</button>
 </template>
